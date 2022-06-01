dda5ec264afa88a7f69dc4899837882eb47f3bb7
Yesaya R <876745@learn.vsb.bc.ca>
2022-06-01 14:50:45 -0700

    more stuff

---
+++ Module 1/Module Assignment/Module 1.4/src/Circle.java
@@ -0,0 +1,14 @@
+public class Circle extends TwoDShape{
+    public final double Pi =3.14;
+
+    private double radius;
+    Circle(double radius){
+
+    }
+
+
+    @Override
+    public double getArea(){
+        return Pi*radius;
+    }
+}
---
+++ Module 1/Module Assignment/Module 1.4/src/TwoDShape.java
@@ -0,0 +1,18 @@
+public abstract class TwoDShape {
+    double width;
+    double height;
+    public TwoDShape(double width, double height) {
+
+    }
+
+    public TwoDShape() {
+
+    }
+
+    public abstract double getArea();
+
+
+    public void setHeight(double height) {
+
+    }
+}
