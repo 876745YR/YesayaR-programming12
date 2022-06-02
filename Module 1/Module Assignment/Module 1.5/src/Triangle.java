115a789d5101682e86c6e6afe060efc8375d4075
Yesaya R <876745@learn.vsb.bc.ca>
2022-06-02 13:56:54 -0700

    1.5

---
+++ Module 1/Module Assignment/Module 1.5/src/Triangle.java
@@ -0,0 +1,24 @@
+public class Triangle extends TwoDShape implements Rotate{
+    double side1;
+    double side2;
+    double side3;
+    public Triangle(double width, double height, Colour color){
+
+    }
+    public Triangle(double side1, double side2, double side3, Colour color){
+
+    }
+    private double heronsHeight(double side1, double side2, double side3){
+        double area;
+        double height;
+        double semiperimeter = (side1+side2+side3/2);
+        area = Math.sqrt(semiperimeter*(semiperimeter-side1)*(semiperimeter-side2)*(semiperimeter-side3));
+        height = 2*area/side2;
+        return height;
+    }
+    public double getArea(){
+
+        return this.side2*heronsHeight(side1,side2,side3);
+
+    }
+}
---
+++ Module 1/Module Assignment/Module 1.5/src/TwoDShape.java
@@ -0,0 +1,20 @@
+public abstract class TwoDShape {
+    double width;
+    double height;
+    Colour colour;
+
+    public TwoDShape(double width, double height, Colour colour) {
+
+    }
+
+    public TwoDShape() {
+
+    }
+
+    public abstract double getArea();
+
+
+    public void setHeight(double height) {
+
+    }
+}
